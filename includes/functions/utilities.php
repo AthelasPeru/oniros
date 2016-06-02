6cb6c19c5cffe09a7d6d5e5341c15573f2acbb15
Renato <hello@renatocenteno.com>
2016-06-02 12:32:42 -0500

    comment changes

--- includes/functions/utilities.php
+++ includes/functions/utilities.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
  *
  * @package Oniros
  * @subpackage Includes
- * @author Elnato
+ * @author elnato
  *
  * @since 1.0.0
  */
@@ -21,7 +21,7 @@ class Oniros_Utils {
 	 * @param  [type] $num_words  
 	 * @return [type]             
 	 */
-	function get_oniros_custom_field_excerpt($field_name, $num_words) 
+	function get_custom_field_excerpt($field_name, $num_words) 
 	{
 		global $post;
 		$text = get_field($field_name);
@@ -67,9 +67,9 @@ class Oniros_Utils {
 	 * @param  [type] $postID [description]
 	 * @return [type]         [description]
 	 */
-	function oniros_set_post_views($postID) 
+	function set_post_views($postID) 
 	{
-	    $count_key = 'oniros_post_views_count';
+	    $count_key = 'post_views_count';
 	    $count = get_post_meta($postID, $count_key, true); //return the value
 	    if ( empty($count) ){
 	        $count = 1;
@@ -83,20 +83,20 @@ class Oniros_Utils {
 	//To keep the count accurate, lets get rid of prefetching
 	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
 
-	function oniros_track_post_views($post_id) 
+	function track_post_views($post_id) 
 	{
 	    if (!is_single()) return;
 	    if (empty($post_id)) {
 	        global $post;
 	        $post_id = $post->ID;    
 	    }
-	    oniros_set_post_views($post_id);
+	    set_post_views($post_id);
 	}
-	add_action( 'wp_head', 'oniros_track_post_views');
+	add_action( 'wp_head', 'track_post_views');
 
-	function oniros_get_post_views($postID)
+	function get_post_views($postID)
 	{
-	    $count_key = 'oniros_post_views_count';
+	    $count_key = 'post_views_count';
 	    $count = get_post_meta($postID, $count_key, true);
 	    if ($count==''){
 	        delete_post_meta($postID, $count_key);
@@ -104,5 +104,75 @@ class Oniros_Utils {
 	        return __("0 Views");
 	    }
 	    return $count.' '.__('Views');
+	}
+
+	public static function pagination_numeric_posts_nav() {
+
+		if( is_singular() )
+			return;
+
+		global $wp_query;
+
+		/** Stop execution if there's only 1 page */
+		if( $wp_query->max_num_pages <= 1 )
+			return;
+
+		$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+		$max   = intval( $wp_query->max_num_pages );
+
+		/**	Add current page to the array */
+		if ( $paged >= 1 )
+			$links[] = $paged;
+
+		/**	Add the pages around the current page to the array */
+		if ( $paged >= 3 ) {
+			$links[] = $paged - 1;
+			$links[] = $paged - 2;
+		}
+
+		if ( ( $paged + 2 ) <= $max ) {
+			$links[] = $paged + 2;
+			$links[] = $paged + 1;
+		}
+
+						// <li><a href="" class=""><i class="fa fa-chevron-right"></i></a></li>
+		echo '<div class="navigation"><ul class="pagination">' . "\n";
+
+		/**	Previous Post Link */
+		if ( get_previous_posts_link() )
+			printf( '<li>%s</li>' . "\n", get_previous_posts_link() );
+
+		/**	Link to first page, plus ellipses if necessary */
+		if ( ! in_array( 1, $links ) ) {
+			$class = 1 == $paged ? ' class="active"' : '';
+
+			printf( '<li%s><span><a href="%s" class="prev">%s</a></span></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
+
+			if ( ! in_array( 2, $links ) )
+				echo '<li>…</li>';
+		}
+
+		/**	Link to current page, plus 2 pages in either direction if necessary */
+		sort( $links );
+		foreach ( (array) $links as $link ) {
+			$class = $paged == $link ? ' class="active"' : '';
+			printf( '<li%s><span><a href="%s" class="next">%s</a></span></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
+		}
+
+		/**	Link to last page, plus ellipses if necessary */
+		if ( ! in_array( $max, $links ) ) {
+			if ( ! in_array( $max - 1, $links ) )
+				echo '<li>…</li>' . "\n";
+
+			$class = $paged == $max ? ' class="active"' : '';
+			printf( '<li%s><span><a href="%s">%s</a></span></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
+		}
+
+		/**	Next Post Link */
+		if ( get_next_posts_link() )
+			printf( '<li>%s</li>' . "\n", get_next_posts_link() );
+
+		echo '</ul></div>' . "\n";
+
 	}
 }
