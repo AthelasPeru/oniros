d54873ad8e50a761615c1d501b2600b75549131e
ondoheer <ondoheer@gmail.com>
2016-12-03 19:19:48 -0500

    Added hooks for to remove static files version

--- includes/functions/security.php
+++ includes/functions/security.php
@@ -9,3 +9,6 @@ function remove_wp_ver_css_js( $src ) {
 		$src = remove_query_arg( 'ver', $src );
 	return $src;
 }
+
+add_filter( 'style_loader_src', 'remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'remove_wp_ver_css_js', 9999 );
