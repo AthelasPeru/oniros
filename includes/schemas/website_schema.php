3e61394f75119ee4e38b122637f9e0eafb5bb6ab
ondoheer <ondoheer@gmail.com>
2016-12-13 08:01:15 -0500

    Added schemas example

---
+++ includes/schemas/website_schema.php
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+	{
+		 "@context": "http://schema.org",
+		 "@type": "WebSite",
+		 "description": "Website description",
+		 "about": "<?php echo get_bloginfo("description" ); ?>",
+		 "author": {   
+                "@type":"person",
+                "name":"name of the author is it's required",
+                "description":"author description",
+                "email": "maud@buenviaje.pe"} ,
+		 "copyrightHolder": "Name of copyright holder",
+		 "creator": "bussiness creator",
+		 "url": "<?php echo site_url(); ?>",
+		 "datePublished": "Y-m-d",		 
+		 "genre": "URL gotten fropm http://vocab.getty.edu/",
+		 "inLanguage": "language short code",
+		 "keywords": "target keywords",
+		 "publisher": "Publisher brand object",
+		 "image": "<?php echo site_url(); ?>/wp-content/themes/athelas/dist/img/logo.png",
+		 "sameAs" : [ 
+		 	"array of links to other sites or social netweorks"
+   		] 	 
+	}
+</script>
