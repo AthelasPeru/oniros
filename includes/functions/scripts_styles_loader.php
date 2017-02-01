12d69d3c3f510f75f82cd405ab16453e76fed403
ondoheer <ondoheer@gmail.com>
2017-02-01 11:23:10 -0500

    * Moved de register scripts to the scripts_styles_loader.php * Reorganized scripts loader function to only change the css to unminified css in debug mode * Added ES6 support with babel, if new script processes need to be added, then a transpile npm script should be created for them as well

--- includes/functions/scripts_styles_loader.php
+++ includes/functions/scripts_styles_loader.php
@@ -43,21 +43,47 @@ function oniros_scripts(){
 		
 		// Our own stylesheet (minimized)
 		wp_enqueue_style('main', get_stylesheet_directory_uri().'/dist/css/main/main.min.css');	
-		
-		// Own script (uglyfied)
-		wp_enqueue_script('main',get_stylesheet_directory_uri().'/dist/js/main.min.js', array('jquery'), null, true);
-		
-		//Add other scripts here
 
 	} else{
 		
 		// Our own stylesheet 
 		wp_enqueue_style('main', get_stylesheet_directory_uri().'/dist/css/main/main.css');	
-		
-		// Own script 
-		wp_enqueue_script('main',get_stylesheet_directory_uri().'/dist/js/main.js', array('jquery'), null, true);
-		
-		// Add other scripts here
-	}
+	}	
+	// Own scripts
+	// main runs on every page load, if you need a specific script to run on a specific page, just create a conditional here
+	wp_enqueue_script('main',get_stylesheet_directory_uri().'/dist/js/main.min.js', array(), null, true);
+	
+	// Add other scripts here
+	
 }
 add_action( 'wp_enqueue_scripts', 'oniros_scripts');
+
+
+
+// REMOVE WP EMOJI http://www.denisbouquet.com/remove-wordpress-emoji-code/
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+// Remove JQuery MIgrate and JQuery if not admin
+add_filter( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+function remove_jquery_migrate( &$scripts)
+{
+    if(!is_admin())
+    {
+     	// removes Jquery (this disables jquery-migrate)
+        $scripts->remove( 'jquery');
+        // Adds just jQuery Core so migrate is excluded
+        // this is commented by default since we think jQuery shouldn't be used unless necesary
+        //$scripts->add( 'jquery', false, array( 'jquery-core' ), '1.10.2' );
+    }
+}
+
+// Remove WP Embedd
+function my_deregister_scripts(){
+  wp_deregister_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'my_deregister_scripts' );
