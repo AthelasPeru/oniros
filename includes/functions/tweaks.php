9021d4caaf2cb9c7c7c4031468454677ea7142ce
Diego Cámere <dcamere@gmail.com>
2017-03-24 13:58:13 -0500

    added svg support

--- includes/functions/tweaks.php
+++ includes/functions/tweaks.php
@@ -11,3 +11,12 @@ function remove_cssjs_ver( $src ) {
 }
 add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
 add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
+
+
+//Enabling upload of svg files
+add_filter('upload_mimes', 'my_upload_mimes');
+ 
+function my_upload_mimes($mimes = array()) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
